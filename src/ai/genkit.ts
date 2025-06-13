d8eb58417a85295b94727ba403b0c68713184d94
MR Carbonell <marioraulcarbonell@gmail.com>
2025-06-13 23:34:48 +0000

    Muy bien, he obtenido mayor variedad de las preguntas con el mismo model

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,6 +4,5 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.0-flash-lite', // Changed to gemini-1.5-pro-latest
+  model: 'googleai/gemini-2.5-flash-preview-05-20',
 });
-
